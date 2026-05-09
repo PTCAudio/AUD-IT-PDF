68b8d32281ebdee61f7691a96a47c7eb9d950ad5
PTCAudio <m.gibney@phoenixtheatre.com>
2026-05-08 17:39:20 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+python
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return"PDF service is alive"
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
+
