b764d4b7a7a24f83fb16f45f84bc436706f1b898
PTCAudio <m.gibney@phoenixtheatre.com>
2026-05-08 20:23:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
+from xhtml2pdf import pisa
+from io import BytesIO
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return"PDF service is alive"
+    return "PDF service is alive"
 
 @app.route("/pdf/test")
 def pdf_test():
-    html = render_templates("test.html")
+    html = render_template("test.html")
     pdf_buffer = BytesIO()
     pisa.CreatePDF(html, dest=pdf_buffer)
-    pdf_buffer,.seek(0)
+    pdf_buffer.seek(0)
     return send_file(
-        mimetype="application/pdf"
+        pdf_buffer,
+        mimetype="application/pdf",
         as_attachment=True,
         download_name="test.pdf"
-)
+    )
 
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
